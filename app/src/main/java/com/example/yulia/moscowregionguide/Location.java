5440355eb43380a174528d5f2e85910c7aaf7b2a
Yulia Gorelikova <yulia.gorelikova1993@gmail.com>
2018-05-09 15:58:29 +0300

    added a custom class for location

---
+++ app/src/main/java/com/example/yulia/moscowregionguide/Location.java
@@ -0,0 +1,37 @@
+package com.example.yulia.moscowregionguide;
+
+public class Location {
+    public String lName;
+    public String lInfo;
+    public String lSite;
+    public String lGeo;
+    public int lPhoto;
+
+    public Location(String name, String info, String site, String geo, int photo) {
+        this.lName = name;
+        this.lInfo = info;
+        this.lSite = site;
+        this.lGeo = geo;
+        this.lPhoto = photo;
+    }
+
+    public String getlName() {
+        return lName;
+    }
+
+    public String getlInfo() {
+        return lInfo;
+    }
+
+    public String getlGeo() {
+        return lGeo;
+    }
+
+    public String getlSite(){
+        return lSite;
+    }
+
+    public int getlPhoto(){
+        return lPhoto;
+    }
+}
